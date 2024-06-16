7064b8d83b82a585bd1a04e614f387c1a411a653
Boris Schäfer <boris.gehring@gehring-online.com>
2024-06-16 18:53:35 +0200

    Initial Commit

---
+++ run4 Watch App/run4App.swift
@@ -0,0 +1,17 @@
+//
+//  run4App.swift
+//  run4 Watch App
+//
+//  Created by Boris on 16.06.24.
+//
+
+import SwiftUI
+
+@main
+struct run4_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
